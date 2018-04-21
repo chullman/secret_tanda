0f8fcc6861683f477c257dce7b29b36515110f0f
chullman <christopherhullman@gmail.com>
2018-04-21 16:56:33 +1000

    randomised activity

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 class HomeController < ApplicationController
   def index
 
+    if user_signed_in?
+      @random_activity = Activity.order("RANDOM()").first
+
+      @new_activity_done = ActivityDone.create(user_id: current_user.id, activity_id: @random_activity.id)
+    end
+
     if !(user_signed_in?)
       redirect_to new_user_session_path
     end
