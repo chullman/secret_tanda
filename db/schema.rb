d17077aa7c490d002d3c72ad5264e436d5da173d
chullman <christopherhullman@gmail.com>
2018-04-21 10:48:18 +1000

    fixed scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180421001645) do
+ActiveRecord::Schema.define(version: 20180421003757) do
 
   create_table "activities", force: :cascade do |t|
     t.string "description"
@@ -29,9 +29,9 @@ ActiveRecord::Schema.define(version: 20180421001645) do
   end
 
   create_table "locations", force: :cascade do |t|
+    t.string "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "city"
   end
 
   create_table "users", force: :cascade do |t|
