986d062c7a7903b7c7fbd03e6a0010483b2238fd
chullman <christopherhullman@gmail.com>
2018-04-21 10:38:30 +1000

    fixed location scaffold

--- test/controllers/locations_controller_test.rb
+++ test/controllers/locations_controller_test.rb
@@ -17,7 +17,7 @@ class LocationsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create location" do
     assert_difference('Location.count') do
-      post locations_url, params: { location: {  } }
+      post locations_url, params: { location: { city: @location.city } }
     end
 
     assert_redirected_to location_url(Location.last)
@@ -34,7 +34,7 @@ class LocationsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update location" do
-    patch location_url(@location), params: { location: {  } }
+    patch location_url(@location), params: { location: { city: @location.city } }
     assert_redirected_to location_url(@location)
   end
 
